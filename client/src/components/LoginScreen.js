08a449c81b2d1f55e7a9d58417f64c80b4982463
taylor-giles <taylor.g1823@gmail.com>
2021-12-01 23:34:06 -0500

    Remove picture from login screen

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -32,21 +32,7 @@ export default function LoginScreen() {
     return (
         <Grid container component="main" sx={{ height: '100vh' }}>
             <CssBaseline />
-            <Grid
-                item
-                xs={false}
-                sm={4}
-                md={7}
-                sx={{
-                    backgroundImage: 'url(https://source.unsplash.com/random)',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundColor: (t) =>
-                        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Grid item xs={12} sm={12} md={12} component={Paper} elevation={6} square>
                 <Box
                     sx={{
                         my: 8,
